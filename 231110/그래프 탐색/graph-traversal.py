e516157c15e4dbf91ba3aa0c02422aa3dfa87729
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-10 15:06:34 +0900

    Title: 그래프 탐색, Time: 200ms, Memory: 29MB, Status: Passed - CodeTree

--- 231110/그래프 탐색/graph-traversal.py
+++ 231110/그래프 탐색/graph-traversal.py
@@ -1,19 +1,12 @@
 N, M = tuple(map(int,input().split()))
-
-start_point = list()
-finish_point =list()
-for _ in range(M):
-    x,y = tuple(map(int,input().split()))
-    start_point.append(x)
-    finish_point.append(y)
-
 graph=[[] for _ in range(N+1)]
 visited=[False for _ in range(N+1)]
 
-for x,y in zip(start_point,finish_point):
+for _ in range(M):
+    x,y = tuple(map(int,input().split()))
     graph[x].append(y)
     graph[y].append(x)
-
+    
 def dfs(vertex):
     global count
     for curr_V in graph[vertex]:
