52945f41ac2c11e437593e692174a9fe774f94d0
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-13 16:35:25 +0900

    Title: k개의 벽 없애기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 231113/k개의 벽 없애기/remove-k-walls.py
+++ 231113/k개의 벽 없애기/remove-k-walls.py
@@ -1,108 +1,117 @@
-# bfs
-# backtracking
-# 매번 거리 비교
-# in_range, can_go, push, reset
-# visited, step
-# min_distacne
-
+import sys
 from collections import deque
-n, k = tuple(map(int,input().split()))
-grid=[
-    list(map(int,input().split()))
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+a = [
+    list(map(int, input().split()))
     for _ in range(n)
 ]
-r1,c1 = tuple(map(int,input().split()))
-r2,c2 = tuple(map(int,input().split()))
 
+r1, c1 = tuple(map(int, input().split()))
+r2, c2 = tuple(map(int, input().split()))
+r1, c1, r2, c2 = r1 - 1, c1 - 1, r2 - 1, c2 - 1
 
-q=deque()
-visited=[
+stone_pos = [
+    (i, j)
+    for i in range(n)
+    for j in range(n)
+    if a[i][j]
+]
+
+# bfs에 필요한 변수들 입니다.
+q = deque()
+visited = [
     [False for _ in range(n)]
     for _ in range(n)
 ]
-
-step=[
-    [0 for _ in range(n)]
-    for _ in range(n)
+step = [                  # step[i][j] : 시작점으로부터 
+    [0 for _ in range(n)] # (i, j) 지점에 도달하기 위한 
+    for _ in range(n)     # 최단거리를 기록합니다.
 ]
-min_distance=2*n
 
-def in_range(x,y):
-    return 0<=x and x<n and 0<=y and y<n
+ans = INT_MAX
 
-def can_go(x,y):
-    if not in_range(x,y):
-        return False
-    if visited[x][y] or grid[x][y]==1:
-        return False
-    return True
 
-def push(x,y,s):
-    visited[x][y]=True
-    step[x][y]=s
-    q.append((x,y))
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def reset(arr):
-    for i in range(n):
-        for j in range(n):
-            visited[i][j]=False
-            step[i][j]=0
 
-    for i in arr:
-        x,y=i
-        grid[x][y]=1
+# 격자를 벗어나지 않으면서, 벽도 없고, 아직 방문한 적이 없는 곳이라면
+# 지금 이동하는 것이 최단거리임을 보장할 수 있으므로 가야만 합니다. 
+def can_go(x, y):
+    return in_range(x, y) and not a[x][y] and not visited[x][y]
 
-    q.clear()
 
+# queue에 새로운 위치를 추가하고
+# 방문 여부를 표시해줍니다.
+# 시작점으로 부터의 최단거리 값도 갱신해줍니다.
+def push(nx, ny, new_step):
+    q.append((nx, ny))
+    visited[nx][ny] = True
+    step[nx][ny] = new_step
+
+
+# bfs를 통해 최소 이동 횟수를 구합니다.
 def bfs():
-    dxs,dys=[1,0,-1,0],[0,1,0,-1]
+    # queue에 남은 것이 없을때까지 반복합니다.
     while q:
-        x,y = q.popleft()
-        for dx,dy in zip(dxs,dys):
-            nx,ny = x+dx, y+dy
-            if can_go(nx,ny):
-                push(nx,ny,step[x][y]+1)
+        # queue에서 가장 먼저 들어온 원소를 뺍니다.
+        x, y = q.popleft()
 
-def remove_wall(arr):
-    for i in arr:
-        x,y=i
-        grid[x][y]=0
+        dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-walls=list()
-removed_wall=list()
+        # queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
 
-def pick_wall(cnt):
-    global min_distance
+            # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+            # 새로 queue에 넣어줍니다.
+            if can_go(nx, ny):
+                # 최단 거리는 이전 최단거리에 1이 증가하게 됩니다. 
+                push(nx, ny, step[x][y] + 1)
 
-    if cnt==k:
-        remove_wall(removed_wall)
-        push(r1-1,c1-1,0)
-        bfs()
-        if visited[r2-1][c2-1]:
-            num=step[r2-1][c2-1]
-            min_distance = min(min_distance,num)
-        reset(removed_wall)
+    # 도착점에 가는 것이 가능할때만
+    # 답을 갱신해줍니다.
+    if visited[r2][c2]:
+        return step[r2][c2]
+    else:
+        return INT_MAX
+
+
+def find_min(idx, cnt):
+    global ans
+    
+    if idx == len(stone_pos):
+        if cnt == k:
+            # visited, step 값을 초기화 해줍니다.
+            for i in range(n):
+                for j in range(n):
+                    visited[i][j] = False
+                    step[i][j] = 0
+            
+		    # bfs를 이용해 최소 이동 횟수를 구합니다.
+		    # 시작점을 queue에 넣고 시작합니다.
+            push(r1, c1, 0)
+            min_dist = bfs()
+            ans = min(ans, min_dist)
+		
         return
 
-    for i in walls:
-        if i in removed_wall:
-            continue
-        removed_wall.append(i)
-        pick_wall(cnt+1)
-        removed_wall.pop()
-    return
-
-#main
-
-for i in range(n):
-    for j in range(n):
-        if grid[i][j]==1:
-            walls.append((i,j))
-
-pick_wall(0)
+    x, y = stone_pos[idx]
+    a[x][y] = 0
+    find_min(idx + 1, cnt + 1)
+    a[x][y] = 1
+    
+    find_min(idx + 1, cnt)
 
 
-if min_distance==2*n:
-    print(-1)
-else:
-    print(min_distance)
+find_min(0, 0)
+
+# 출력:
+if ans == INT_MAX:  # 불가능한 경우라면
+    ans = -1        # -1을 답으로 넣어줍니다.
+
+print(ans)
