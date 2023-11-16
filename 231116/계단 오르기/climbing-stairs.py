750615bb35d0e41d062714dc341330fce54505ee
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-16 16:59:32 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231116/계단 오르기/climbing-stairs.py
@@ -0,0 +1,22 @@
+# 5까지 가는 방법은 3까지 가는 방법 + 2까지 가는 방법이다.
+# = f(5) = f(3) + f(2)
+# f(n) = f(n-2) + f(n-3)
+
+#f(2) =1, f(3) =1
+
+n=int(input())
+memo=[-1 for _ in range(n+1)]
+
+def calculate_stair(n):
+    if memo[n]!=-1:
+        return memo[n]
+    if memo[n]==-1:
+        memo[n] = calculate_stair(n-3) + calculate_stair(n-2)
+
+    return memo[n]
+if(n==2):
+    print(1)
+else:
+    memo[2]=1
+    memo[3]=1
+    print(calculate_stair(n))
