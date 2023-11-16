af8a5c010245e1afef6f0f06d487f828718b02be
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-16 23:53:12 +0900

    Title: 서로 다른 BST 개수 세기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 231116/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -1,14 +1,12 @@
 n=int(input())
 dp=[-1 for _ in range(20)]
-
+dp[0] = 1
 dp[1] = 1
 dp[2] = 2
 for i in range(3,n+1):
     sum=0
-    for j in range(1,i//2+1):
-        sum+=dp[i-j]*2
-    if i%2==1:
-        sum+=dp[i//2]*dp[i//2]
+    for j in range(1,i+1):
+        sum+=dp[j-1]*dp[i-j]
     dp[i]=sum
 
 print(dp[n])
