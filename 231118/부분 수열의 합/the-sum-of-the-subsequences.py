2560e14839cc90ae847ecf310e53d9c12a76b03b
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-18 19:43:40 +0900

    Title: 부분 수열의 합, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231118/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,22 @@
+# 합이 되나 안되냐
+# 중복x
+# dp[i] 정의 - 합i를 부분 수열로 만들수 있는지 없는지
+# 점화식 - dp[i-arr[j]]이 True면 True
+# 초기조건 dp[0] = true
+# 조건 i>=arr[j]
+
+n,m = tuple(map(int,input().split()))
+arr = list(map(int,input().split()))
+dp=[False for _ in range(m+1)]
+dp[0] = True
+
+for i in range(n):
+    for j in range(m,-1,-1):
+        if j>=arr[i]:
+            dp[j] = dp[j-arr[i]]
+
+# print(dp)
+if dp[m]:
+    print("Yes")
+if not dp[m]:
+    print("No")
