dcef90b3388bc4890cf63a1dbd310a64d9e2769c
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-11-18 18:39:53 +0900

    Title: 부분 수열의 합이 m, Time: 208ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231118/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,22 @@
+n, m =tuple(map(int,input().split()))
+arr=[0] + list(map(int,input().split()))
+dp = [0 for _ in range(m+1)]
+
+def initialize():
+    for i in range(m+1):
+        dp[i] = 101
+    dp[0]=0
+
+initialize()
+
+for i in range(1,n+1):
+    for j in range(m,-1,-1):
+        if j>=arr[i]:
+            if dp[j-arr[i]]==101:
+                continue
+            dp[j] = min(dp[j],dp[j-arr[i]]+1)
+
+if dp[m]==101:
+    print(-1)
+else:
+    print(dp[m])
