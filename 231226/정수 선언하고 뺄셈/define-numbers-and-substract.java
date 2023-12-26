9b1cfeebef56c88cad0ec3f1649cf527ee5f7ecc
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-26 21:12:49 +0900

    Title: 정수 선언하고 뺄셈, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231226/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 97;
+        int secondNumber = 13;
+        int ans = firstNumber - secondNumber;
+        System.out.println(firstNumber+" - "+secondNumber+" = "+ans);
+    }
+}
