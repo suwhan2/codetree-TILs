ed2b0a77fde016b1c815c8ea173a5e8ba7fbe7dd
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-26 12:29:56 +0900

    Title: 두줄 출력, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 231226/두줄 출력/print-two-sentences-introduce.java
+++ 231226/두줄 출력/print-two-sentences-introduce.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello student!");
-        System.out.print("Welcojme to LeebrosCode!");// 여기에 코드를 작성해주세요.
+        System.out.println("Hello students!");
+        System.out.print("Welcome to LeebrosCode!");// 여기에 코드를 작성해주세요.
     }
 }
