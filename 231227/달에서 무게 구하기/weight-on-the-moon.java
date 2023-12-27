5490248b402ee9a3b1aaf9593f5ad7feda2cadb9
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-27 16:06:48 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231227/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,11 @@
+//13 * 0.165000 = 2.145000
+
+public class Main {
+    public static void main(String[] args) {
+        double weight = 13;
+        double moonWeight = 0.165;
+        double ans = weight*moonWeight;
+        System.out.printf("%.0f * %.6f = %.6f",weight,moonWeight,ans);
+            // 여기에 코드를 작성해주세요.
+    }
+}
