216bc833a7887e997da4e730429dafccfc6892bf
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-27 15:35:43 +0900

    Title: 변수 출력하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231227/변수 출력하기/outputing-variables.java
@@ -0,0 +1,10 @@
+//3...C
+
+public class Main {
+    public static void main(String[] args) {
+        int a=3;
+        char b = 'C';
+        System.out.print(a+"..."+b);
+        // 여기에 코드를 작성해주세요.
+    }
+}
