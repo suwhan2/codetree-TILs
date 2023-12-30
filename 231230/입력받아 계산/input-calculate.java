8013c964123e246d49a8476bfe354f1b7a6730ba
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-30 17:59:29 +0900

    Title: 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231230/입력받아 계산/input-calculate.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    a=a+2;
+        System.out.println(a);
+    }
+    }
+}
