d5226f583c105f3cce47732a4386e0bfc754ec00
SuHwan Choi <105771340+suwhan2@users.noreply.github.com>
2023-12-30 20:36:57 +0900

    Title: 입력받아 출력 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 231230/입력받아 출력 2/input-and-output-2.java
+++ 231230/입력받아 출력 2/input-and-output-2.java
@@ -6,6 +6,6 @@ public class Main {
         sc.useDelimiter("-"); // - 기준으로 잘라서 입력 받겠다는 뜻
         int a = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
         int b = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
-        System.out.println(a+b);
+        System.out.println(a+""+b);
     }
 }
