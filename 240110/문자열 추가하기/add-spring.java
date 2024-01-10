610e0d84a6484b6ff748b0a862e12c4aa05fc395
ChoiSuHwan <jerry5230@naver.com>
2024-01-10 21:53:37 +0900

    Title: 문자열 추가하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/문자열 추가하기/add-spring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //입력부
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = "Hello";
+        System.out.println(a+b);
+    }
+}
