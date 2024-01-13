1b14e8cc070140e947a00b561655446883e8659b
ChoiSuHwan <jerry5230@naver.com>
2024-01-13 20:40:47 +0900

    Title: 대문자로 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240113/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String strInput = sc.next();
+
+        int n =strInput.length();
+        char arr[] = new char[n];
+        arr = strInput.toCharArray();
+
+        int gap = 'a'-'A';
+        for(int i=0;i<n;i++){
+            if('A'<=arr[i]&&arr[i]<='Z'){
+                System.out.print(arr[i]);
+            }
+            else if('a'<=arr[i]&&arr[i]<='z'){
+                arr[i]-=gap;
+                System.out.print(arr[i]);
+            }
+        }
+
+    }
+}
