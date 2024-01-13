9e93eb9c37decfa7b29835f5505e2b2da3f957ab
ChoiSuHwan <jerry5230@naver.com>
2024-01-13 22:01:54 +0900

    Title: 정수만 더하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240113/정수만 더하기/add-only-integers.java
+++ 240113/정수만 더하기/add-only-integers.java
@@ -6,12 +6,10 @@ public class Main {
         String strInput = sc.next();
 
         int n = strInput.length();
-        char arr[] = new char[n];
-        arr = strInput.toCharArray();
         int ans=0;
         for (int i = 0; i < n; i++) {
-            if ('0' <= arr[i] && arr[i] <= '9') {
-                ans+=arr[i];
+            if ('0' <= strInput.charAt(i) && strInput.charAt(i) <= '9') {
+                ans+=(strInput.charAt(i)-'0');
             }
         }
         System.out.println(ans);
