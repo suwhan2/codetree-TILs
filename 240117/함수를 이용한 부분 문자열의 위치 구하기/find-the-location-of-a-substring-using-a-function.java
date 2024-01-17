3f896953de061e35b919f5f5dff5267ec512d977
ChoiSuHwan <jerry5230@naver.com>
2024-01-17 21:44:56 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String strInput, desStr;
+
+    public static boolean compareTwoStr(int n){
+        for(int i=n;i<n+desStr.length();i++){
+            if(strInput.charAt(i)!=desStr.charAt(i-n)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int checkInvolveArr(){
+        int ans =-1;
+        for(int i=0;i<strInput.length();i++){
+            if(compareTwoStr(i)){
+                ans=i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        strInput = sc.next();
+        desStr = sc.next();
+
+        System.out.println(checkInvolveArr());
+
+    }
+}
